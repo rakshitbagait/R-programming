505afbba692020e620b2bb09bf67562ca4c526a9
rakshitbagait <rbagait2018@gmail.com>
2025-07-08 22:44:00 +0530

    basics of r

---
+++ comments.r
@@ -0,0 +1,14 @@
+# the single line comments in r is made up using "#"
+
+print("My name is rakshit")
+print("hello world")
+a= 14/7
+print(a)
+
+my_vec = c(1,3,1,42)
+print(my_vec)
+
+foo <- 5.3
+print(foo)
+bar <- foo:(-47+1.5)
+print(bar)
---
+++ vectors.r
@@ -0,0 +1,8 @@
+# these are the functions associated with the r vector
+
+my_vec = c(1,2,3,35,632)
+print(my_vec)
+
+sequence = seq(2,56,5)
+print(sequence)
+
