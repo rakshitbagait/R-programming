0af4daae67d5b788b80eda599439a33a426ecfbe
rakshitbagait <rbagait2018@gmail.com>
2025-07-28 22:46:22 +0530

    subsetting in R

---
+++ chapter 2/subsetting.R
@@ -0,0 +1,15 @@
+# negative indexes
+
+
+myvec <- c(1,23,52,42,252,52353,5253,5252,76363,36346346,644)
+ 
+
+print(myvec[-2])
+print(myvec)
+print(myvec[c(1,3,4,6,7)])
+print("this is the 1 to five")
+print(myvec[1:5])
+
+foo = 5.5:0.5
+print(foo)
+print()
