79b959367eb2620367443530bbacc8386e76f86b
rakshitbagait <rbagait2018@gmail.com>
2025-08-01 23:27:38 +0530

    chapter 2 finished

--- chapter 2/vectorize.R
+++ chapter 2/vectorize.R
@@ -1,4 +1,10 @@
 foo = 5.5:0.5
-print(foo)
-print( foo-c(2,4,6,8,10,12))
+# print(foo)
+# print( foo-c(2,4,6,8,10,12))
 
+vec2 = c(4,32,52,53,53,4,523,52)
+
+n_vec = vec2*foo
+n2_vec = foo*vec2
+print(n_vec)
+print(n2_vec)
