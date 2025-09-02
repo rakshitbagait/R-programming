64c4f47eac4b71ae889c79bc73a979912c54c590
rakshitbagait <rbagait2018@gmail.com>
2025-09-02 10:18:31 +0530

    data frame in r

---
+++ chapter 5/dataFrame.R
@@ -0,0 +1,17 @@
+# data frame of in R 
+
+# construction of the data frame
+
+data = data.frame(person = c("Rakshit","John","Peter"),age=c(23,52,32),sex=c(T,F,T))
+print(data)
+
+# adding data colums and combining them
+
+new_row = data.frame(person="Brian",age = 32,sex=factor(T,levels=levels(data$sex)))
+data= rbind(data,new_row)
+print(data)
+
+new_colum = data.frame(exp=c("fresher","2nd","3rd","fresher"))
+data = cbind(data,new_colum)
+print(data)
+
