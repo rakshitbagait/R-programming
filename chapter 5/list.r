a01b72db66a2ca0565418024e3b587e5cdd6e5a1
rakshitbagait <rbagait2018@gmail.com>
2025-10-08 10:52:03 +0530

    list

--- chapter 5/list.r
+++ chapter 5/list.r
@@ -23,3 +23,37 @@ print(student$name)
 
 print(student[["marks"]])
 
+my_list <- list(10,"Hello",TRUE,3.14)
+print(my_list)
+
+# named list
+
+person <- list(name = "Rakshit",age=42,skills=c("Python","R","Sql"))
+print(person)
+
+# accessing by name
+
+person$name
+person[["age"]]
+print(person[1])
+
+# the index of the R list starts with 1......
+
+# Adding a new element
+
+person$marks<-c(23,42,53)
+print(person)
+# updating list 
+# method 1
+
+person$age <- 20
+
+# method 2
+person[[2]]<-21
+
+# method 3
+
+person[["age"]]<-19
+
+
+print(person[["age"]])
