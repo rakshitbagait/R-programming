fbeb0bf84eff301d172c54837c97ae76def76968
rakshitbagait <rbagait2018@gmail.com>
2025-10-11 10:31:10 +0530

    data frame

--- chapter 5/dataFrame.R
+++ chapter 5/dataFrame.R
@@ -14,4 +14,48 @@ new_colum = data.frame(exp=c("fresher","2nd","3rd","fresher"))
 data = cbind(data,new_colum)
 print(data)
 
-        
+# to know the struct of a data frame 
+
+# print(str(data))
+
+# Using data.frame()
+students <- data.frame(
+  name = c("Amit", "Sneha", "Ravi", "Priya"),
+  age = c(20, 21, 19, 22),
+  marks = c(85.5, 90.0, 76.5, 88.0),
+  passed = c(TRUE, TRUE, FALSE, TRUE)
+)
+
+print(students)
+str(students)
+
+
+# By default, R converts strings to factors (in older versions).
+
+students <- data.frame(
+  name = c("Amit", "Sneha", "Ravi", "Priya"),
+  age = c(20, 21, 19, 22),
+  marks = c(85.5, 90.0, 76.5, 88.0),
+  passed = c(TRUE, TRUE, FALSE, TRUE),
+  stringsAsFactors = FALSE
+)
+
+print(students)
+
+# creating data frame from existing vectors
+
+name <- c("John","Jane","Jim")
+score <- c(90,85,95)
+passed <- c (T,T,T)
+df <- data.frame(name,score,passed)
+print(df)
+
+
+# from a list 
+lst <- list(id = 1:3,city =c("Delhi","Mumbai","Kolkata"))
+df <- as.df.frame(lst)
+
+
+# using built - in datasets 
+
+data()
